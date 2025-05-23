12e802a9ccb0a8a692655ccc07b574e274f4321f
Luiza Sales <154068580+luizawander@users.noreply.github.com>
2025-05-22 22:40:42 -0300

    migrations

--- APICatalogo1/Models/Categoria.cs
+++ APICatalogo1/Models/Categoria.cs
@@ -1,10 +1,18 @@
-﻿namespace APICatalogo1.Models;
+﻿using System.Collections.ObjectModel;
+
+namespace APICatalogo1.Models;
 
 public class Categoria
 {
+    public Categoria() // inicializando coleção
+    {
+        Produtos = new Collection<Produto>();
+    }
     public int CategoriaId { get; set; }
     public String? Nome { get; set; }
     public String? ImagemUrl { get; set; }
+    
+    public ICollection<Produto> Produtos { get; set; } // 1 para muitos
 }
 
 
