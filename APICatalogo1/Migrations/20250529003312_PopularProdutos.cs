2c82eabce0e6fe65d381af649f8bd7f88ea8d138
Luiza Sales <154068580+luizawander@users.noreply.github.com>
2025-05-28 22:03:09 -0300

    populando tables

---
+++ APICatalogo1/Migrations/20250529003312_PopularProdutos.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APICatalogo1.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopularProdutos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder mb)
+        {
+            mb.Sql("Insert into Categorias(Nome, Preco,ImagemUrl,Estoque,DataCadastro,CategoriaId) " +
+                "Values('Coca-Cola','Regrigerante de Cola 350ml',5.45,'cocacola.jpg',50,now(),1");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder mb)
+        {
+            mb.Sql("Delete From Produtos");
+        }
+    }
+}
--- APICatalogo1/Models/Categoria.cs
+++ APICatalogo1/Models/Categoria.cs
@@ -1,15 +1,26 @@
 ﻿using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 
 namespace APICatalogo1.Models;
 
+[Table("Categorias")] // nome da tabela no banco de dados
 public class Categoria
 {
     public Categoria() // inicializando coleção
     {
         Produtos = new Collection<Produto>();
     }
+    [Key]
     public int CategoriaId { get; set; }
+
+    [Required]
+    [StringLength(80)]
     public String? Nome { get; set; }
+
+    [Required]
+    [StringLength(300)]
     public String? ImagemUrl { get; set; }
     
     public ICollection<Produto> Produtos { get; set; } // 1 para muitos
